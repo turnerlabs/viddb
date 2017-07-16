744236cc2a689f5b2ce24d2fdf25becbeefa8838
Don Browning <don.browning@gmail.com>
2017-07-16 00:26:17 -0400

    refinements

--- public/js/site.js
+++ public/js/site.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 
+    google.charts.load("current", {packages:["corechart", "bar"]});
+    //google.charts.setOnLoadCallback(drawChart);
 
     $.getJSON('/api/getVideoList', function(videoResult){
         
@@ -23,17 +25,16 @@ function getVideoInfo(e){
     $.getJSON('/api/getLabelCount/' + vidName, function(listResult){
         var labels = listResult.res;
 
-        console.log(labels);
-
         $('#video-content').empty();
         $('#wordcloud').empty();
 
-        
+        /*
         $('#video-content').append(
             $('<pre>').text(
                 JSON.stringify(labels, null, '  ')
             )
         );
+        */
 
         // {text: 'bumble', size: 29, href: 'https://en.wikipedia.org/wiki/Beadle'}
         var newLabels = labels.map(function(obj){
@@ -43,7 +44,7 @@ function getVideoInfo(e){
             return n;
         });
         d3.wordcloud()
-            .size([500, 300])
+            .size([500, 400])
             .fill(d3.scale.ordinal().range(["#884400", "#448800", "#888800", "#444400"]))
             .words(newLabels)
             .onwordclick(function(d, i) {
@@ -52,7 +53,32 @@ function getVideoInfo(e){
             .start();
 
         /* histobram */
-        
+        drawHistogram(labels);
     });
 
+}
+
+
+function drawHistogram(labelData) {
+    var maxLength = labelData.length > 20 ? 20 : labelData.length;
+
+    var chartData = [['Label', 'Total']];
+    for(i=0;i<maxLength;i++){
+        var l = labelData[i].Label;
+        var t = labelData[i].total;
+        var lt = [l, t];
+        chartData.push(lt);
+    }
+
+    var data = google.visualization.arrayToDataTable(chartData);
+
+    var options = {
+        title: 'Label Distribution',
+        legend: { position: 'none' },
+        vAxis: {title: 'Label Data'},
+        bars: 'horizontal'
+    };
+
+    var chart = new google.charts.Bar(document.getElementById('chart_div'));
+    chart.draw(data, options);
 }
